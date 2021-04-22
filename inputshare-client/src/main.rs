f3860810ac5aa96512ad2788813da94d3add1b56
sidit77 <sidit77@gmail.com>
2021-04-22 16:49:30 +0200

    swapped to tcp as network protocol

--- inputshare-client/src/main.rs
+++ inputshare-client/src/main.rs
@@ -3,14 +3,12 @@ extern crate bitflags;
 extern crate native_windows_gui as nwg;
 
 use crate::inputhook::InputEvent;
-use crate::keys::{HidModifierKeys, KeyState, convert_win2hid, HidScanCode, VirtualKey};
+use crate::keys::{HidModifierKeys, KeyState, convert_win2hid, HidScanCode};
 use crate::gui::SystemTray;
 use nwg::NativeUi;
-use laminar::{Socket, Packet};
-use std::time::Instant;
-use std::net::{ToSocketAddrs, SocketAddr};
-use crate::hookv2::{InputHook};
-use std::ops::{Deref, DerefMut};
+use std::time::Duration;
+use std::net::{ToSocketAddrs, SocketAddr, TcpStream, Shutdown};
+use std::io::{Write, Read};
 
 mod gui;
 mod inputhook;
@@ -18,85 +16,169 @@ mod keys;
 mod config;
 mod hookv2;
 
-fn main() {
+fn main(){
     println!("Hello client!");
 
-    {
-        //let mut counter = 0;
-        //let _hook = hookv2::InputHook::create(|event| match event{
-        //    InputEvent::KeyboardEvent(key, _, state) => match state {
-        //        KeyState::Pressed => {
-        //            counter += 1;
-        //            println!("{} buttons pressed", counter);
-        //            //if matches!(key, VirtualKey::Escape) {
-        //            //    nwg::stop_thread_dispatch();
-        //            //}
-        //            true
-        //        },
-        //        KeyState::Released => true
-        //    }
-        //});
-        //nwg::dispatch_thread_events();
-        let mut a = 0;
-        let mut b = 0;
-        {
-            let _p1 = InputHook::new(|event| {
-                a += 1;
-                println!("1: I was called {} times!", &a);
-                return true;
-            });
-
-            println!("== START 1 ==");
-            nwg::dispatch_thread_events();
-            println!("== END 1 ==");
-
-            {
-                let _p2 = InputHook::new(|event| {
-                    b += 1;
-                    println!("2: I was called {} times!", &b);
-                    return false;
-                });
-
-                println!("== START 2 ==");
-                nwg::dispatch_thread_events();
-                println!("== END 2 ==");
-            }
-            //(&mut *p.callback.borrow_mut())();
-            //(p.callback.deref().borrow_mut().deref_mut())();
-
-            println!("== START 3 ==");
-            nwg::dispatch_thread_events();
-            println!("== END 3 ==");
-        }
-        println!("counter {}, {}", a, b);
-    }
-
-    println!("Goodbye client!");
-    return;
     let cfg = config::Config::load();
 
     nwg::init().expect("Failed to init Native Windows GUI");
     let _ui = SystemTray::build_ui(Default::default()).expect("Failed to build UI");
 
-    let mut socket = Socket::bind(&cfg.local_address).unwrap();
-    let (sender, _) = (socket.get_packet_sender(), socket.get_event_receiver());
+    let server = match std::env::var("REMOTE_OVERRIDE") {
+        Ok(s) => s.parse().expect("Can not parse address given with REMOTE_OVERRIDE"),
+        Err(_) => cfg.remote_address
+            .to_socket_addrs()
+            .expect("Unable to resolve domain")
+            .filter(|x|match x {
+                SocketAddr::V4(_) => true,
+                SocketAddr::V6(_) => false
+            })
+            .next()
+            .expect("Can not find suitable address!")
+    };
+
+    match TcpStream::connect(server) {
+        Ok(mut stream) => {
+            match do_handshake(&mut stream){
+                Ok(_) => {
+                    println!("Successfully connected to server");
+                    run(&mut stream);
+                    stream.shutdown(Shutdown::Both).unwrap();
+                },
+                Err(_) => {
+                    println!("An handshake error occurred, terminating connection");
+                    stream.shutdown(Shutdown::Both).unwrap();
+                }
+            }
+        }
+        Err(e) => println!("Failed to connect: {}", e)
+    }
+}
+
+fn do_handshake(stream: &mut TcpStream) -> anyhow::Result<()> {
+    println!("Starting handshake");
+    let mut data = [0 as u8; 50];
+    stream.set_read_timeout(Some(Duration::from_secs(3)))?;
+    //stream.read_line(&mut buffer)?;
+    let buffer = read_string(stream, &mut data)?;
+    println!("Got: {}", buffer.trim());
+    if buffer.trim() != "Authenticate" {
+        anyhow::bail!("Wrong protocol!");
+    }
+    stream.write_all(b"secretPassword\n")?;
+    //stream.read_line(&mut buffer)?;
+    let buffer = read_string(stream, &mut data)?;
+    println!("Got: {}", buffer.trim());
+    if buffer.trim() != "Ok" {
+        anyhow::bail!("Wrong protocol!");
+    }
+    Ok(())
+}
+
+fn read_string(stream: &mut TcpStream, data: &mut [u8]) -> anyhow::Result<String> {
+    let read = stream.read(data)?;
+    Ok(String::from_utf8_lossy(&data[0..read]).to_string())
+}
+
+fn run(stream: &mut TcpStream) {
+    let mut modifiers = HidModifierKeys::None;
+    let mut pressed_keys = Vec::<HidScanCode>::new();
+    let _hook = hookv2::InputHook::new(|event|{
+        match event {
+            InputEvent::KeyboardEvent(key, scancode, state) => {
+                let fresh = match HidModifierKeys::from_virtual_key(&key) {
+                    Some(m) => {
+                        let old = modifiers;
+                        match state {
+                            KeyState::Pressed => modifiers.insert(m),
+                            KeyState::Released => modifiers.remove(m)
+                        }
+                        modifiers != old
+                    }
+                    None => match convert_win2hid(&scancode) {
+                        Some(hid) => match state {
+                            KeyState::Pressed => match pressed_keys.contains(&hid) {
+                                false => {
+                                    pressed_keys.push(hid);
+                                    true
+                                },
+                                true => false
+                            }
+                            KeyState::Released => match pressed_keys.iter().position(|x| *x == hid) {
+                                Some(index) => {
+                                    pressed_keys.remove(index);
+                                    true
+                                },
+                                None => false
+                            }
+                        }
+                        None => {
+                            println!("Unsupported key: {:?} ({:x?})", key, scancode);
+                            false
+                        }
+                    }
+                };
+
+                if fresh{
+                    let mut packet: [u8; 8] = [0; 8];
+                    packet[0] = modifiers.to_byte();
+                    for i in 0..pressed_keys.len().min(6){
+                        packet[2 + i] = pressed_keys[0.max(pressed_keys.len() as i32 - 6) as usize + i];
+                    }
+                    //println!("{:x?}", packet);
+                    //sender.send(Packet::reliable_unordered(server, Vec::from(packet))).unwrap();
+                    stream.write_all(&packet).expect("Error sending packet");
+                    //println!("{:?} - {:x?}", modifiers, pressed_keys);
+                }
+
+                false
+            }
+        }
+    });
+
+    nwg::dispatch_thread_events();
+
+    //nwg::dispatch_thread_events_with_callback(move ||{
+    //    //socket.manual_poll(Instant::now());
+    //    //receiver.try_recv();
+    //});
+}
+
+/*
+fn main() {
+    println!("Hello client!");
+
+    let cfg = config::Config::load();
+
+    nwg::init().expect("Failed to init Native Windows GUI");
+    let _ui = SystemTray::build_ui(Default::default()).expect("Failed to build UI");
+
+    let mut socket = Socket::bind_with_config(&cfg.local_address, Config{
+        heartbeat_interval: Some(Duration::from_secs(1)),
+        ..Default::default()
+    }).unwrap();
+    let (sender,receiver) = (socket.get_packet_sender(), socket.get_event_receiver());
     println!("Connected on {}", socket.local_addr().unwrap());
 
-    let server = cfg.remote_address
-        .to_socket_addrs()
-        .expect("Unable to resolve domain")
-        .filter(|x|match x {
-            SocketAddr::V4(_) => true,
-            SocketAddr::V6(_) => false
-        })
-        .next()
-        .expect("Can not find suitable address!");
+
+    let server = match std::env::var("REMOTE_OVERRIDE") {
+        Ok(s) => s.parse().expect("Can not parse address given with REMOTE_OVERRIDE"),
+        Err(_) => cfg.remote_address
+            .to_socket_addrs()
+            .expect("Unable to resolve domain")
+            .filter(|x|match x {
+                SocketAddr::V4(_) => true,
+                SocketAddr::V6(_) => false
+            })
+            .next()
+            .expect("Can not find suitable address!")
+    };
 
     println!("Connecting to {}", server);
 
     let mut modifiers = HidModifierKeys::None;
     let mut pressed_keys = Vec::<HidScanCode>::new();
-    inputhook::set_up_keyboard_hook(move |event|{
+    let _hook = hookv2::InputHook::new(|event|{
         match event {
             InputEvent::KeyboardEvent(key, scancode, state) => {
                 let fresh = match HidModifierKeys::from_virtual_key(&key) {
@@ -148,12 +230,11 @@ fn main() {
         }
     });
 
-
     nwg::dispatch_thread_events_with_callback(move ||{
         socket.manual_poll(Instant::now());
+        receiver.try_recv();
     });
 
-    inputhook::release_hook();
 /*
     let addr = "127.0.0.1:12352";
     let mut socket = Socket::bind(addr).unwrap();
@@ -197,3 +278,5 @@ fn main() {
 
     */
 }
+
+*/
--- inputshare-server/src/main.rs
+++ inputshare-server/src/main.rs
@@ -1,11 +1,104 @@
 mod config;
 
-use laminar::{SocketEvent, Socket};
-use std::thread;
 use std::fs::{OpenOptions};
-use std::io::{Write};
+use std::io::{Write, Read};
 use std::env;
+use std::time::Duration;
+use std::net::{TcpListener, TcpStream, Shutdown};
 
+
+fn main(){
+
+    println!("Starting server!");
+
+    let cfg = config::Config::load();
+    let mut file = match env::args().nth(1) {
+        None => {
+            println!("Using console as backend!");
+            None
+        },
+        Some(path) => {
+            println!("Writing into {}", &path);
+            Some(OpenOptions::new()
+                .write(true)
+                .append(true)
+                .open(path)
+                .expect("can not open device!"))
+        }
+    };
+
+    //run(Transport::FramedTcp, "0.0.0.0:12345".to_socket_addrs().unwrap().next().unwrap())
+    let listener = TcpListener::bind(&cfg.local_address).unwrap();
+    println!("Server listens on {}", listener.local_addr().unwrap());
+
+    loop {
+        println!("Waiting for incoming connections");
+        match listener.accept(){
+            Ok((mut stream, addr)) => {
+                println!("Got connection from {}", addr);
+
+                match do_handshake(&mut stream) {
+                    Ok(_) => {
+                        stream.set_read_timeout(None).unwrap();
+                        let mut data = [0 as u8; 50];
+                        loop {
+                            match stream.read(&mut data) {
+                                Ok(size) => {
+                                    if size == 0 {
+                                        break;
+                                    }
+                                    let msg = &data[0..size];
+                                    // echo everything!
+                                    //stream.write(&data[0..size]).unwrap();
+                                    match file.as_mut() {
+                                        None => println!("Received {:?} from {:?}", &msg, &addr),
+                                        Some(device) => match device.write(&msg) {
+                                            Ok(_) => {},
+                                            Err(e) => println!("Encountered error while write packet {:?} into file {:?}:\n{}", &msg, &device, e)
+                                        }
+                                    }
+                                },
+                                Err(_) => {
+                                    println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+                                    stream.shutdown(Shutdown::Both).unwrap();
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    Err(_) => {
+                        println!("An handshake error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+                        stream.shutdown(Shutdown::Both).unwrap();
+                    }
+                }
+
+            }
+            Err(e) => println!("An error occurred!\n{}", e)
+        }
+    }
+
+}
+
+fn do_handshake(stream: &mut TcpStream) -> anyhow::Result<()> {
+    let mut data = [0 as u8; 50];
+    //let mut buffer = String::new();
+    stream.set_read_timeout(Some(Duration::from_secs(3)))?;
+    stream.write_all(b"Authenticate\n")?;
+    let buffer = read_string(stream, &mut data)?;
+    println!("Got: {}", buffer.trim());
+    if buffer.trim() != "secretPassword" {
+        anyhow::bail!("Wrong password!");
+    }
+    stream.write_all(b"Ok\n")?;
+    Ok(())
+}
+
+fn read_string(stream: &mut TcpStream, data: &mut [u8]) -> anyhow::Result<String> {
+    let read = stream.read(data)?;
+    Ok(String::from_utf8_lossy(&data[0..read]).to_string())
+}
+
+/*
 fn main() {
     println!("Starting server!");
 
@@ -25,8 +118,11 @@ fn main() {
         }
     };
 
-    let mut socket = Socket::bind(&cfg.local_address).unwrap();
-    let (_, receiver) = (socket.get_packet_sender(), socket.get_event_receiver());
+    let mut socket = Socket::bind_with_config(&cfg.local_address, Config{
+        heartbeat_interval: Some(Duration::from_secs(1)),
+        ..Default::default()
+    }).unwrap();
+    let (sender, receiver) = (socket.get_packet_sender(), socket.get_event_receiver());
     println!("Running on {:?}", socket.local_addr().unwrap());
     let _thread = thread::spawn(move || socket.start_polling());
 
@@ -51,6 +147,8 @@ fn main() {
                         }
                     }
 
+                    sender.send(Packet::unreliable(packet.addr(),  "OK".as_bytes().to_vec())).expect("This should send");
+
                     //sender
                     //    .send(Packet::reliable_unordered(
                     //        packet.addr(),
@@ -61,8 +159,14 @@ fn main() {
                 SocketEvent::Timeout(address) => {
                     println!("Client timed out: {}", address);
                 }
-                _ => {}
+                SocketEvent::Connect(address) => {
+                    println!("Client connected from: {}", address);
+                }
+                SocketEvent::Disconnect(address) => {
+                    println!("Client disconnected from: {}", address);
+                }
             }
         }
     }
 }
+*/
