fb79aef1771f7e39ae2f1bef2e2784b129d62e88
sidit77 <sidit77@gmail.com>
2021-05-08 09:25:37 +0200

    simplified layout

--- inputshare-gui/src/main.rs
+++ inputshare-gui/src/main.rs
@@ -1,4 +1,4 @@
-use iced::{button, Align, Button, Column, Element, Sandbox, Settings, Text, Row, Length, VerticalAlignment, Color, Background, Container, Space, HorizontalAlignment, Scrollable, scrollable};
+use iced::{button, Align, Button, Column, Element, Sandbox, Settings, Text, Row, Length, VerticalAlignment, Color, Background, Container, Space, HorizontalAlignment, Scrollable, scrollable, TextInput};
 use iced::container::Style;
 use iced::scrollable::Scrollbar;
 
@@ -8,7 +8,8 @@ pub fn main() -> iced::Result {
 
 enum ContainerStyle {
     Colored(Color),
-    Border
+    Border,
+    Text
 }
 
 impl iced::container::StyleSheet for ContainerStyle {
@@ -22,6 +23,10 @@ impl iced::container::StyleSheet for ContainerStyle {
                 border_width: 2.0,
                 border_radius: 4.0,
                 border_color: Color::from_rgb8(230,230,230),
+                ..Default::default()
+            },
+            ContainerStyle::Text => Style {
+                background: Some(Background::from(Color::from_rgb8(230,230,230))),
                 ..Default::default()
             }
         }
@@ -40,7 +45,7 @@ struct Counter {
     server_output: scrollable::State
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 enum Message {
     IncrementPressed,
     DecrementPressed,
@@ -51,7 +56,7 @@ impl Sandbox for Counter {
 
     fn new() -> Self {
         Self{
-            lines: std::fs::read_to_string("inputshare-server/src/main.rs")
+            lines: std::fs::read_to_string("test.log")
                 .expect("file not found!")
                 .lines()
                 .map(|s|String::from(s))
@@ -80,86 +85,52 @@ impl Sandbox for Counter {
             .padding(10)
             .spacing(8)
             .push(
+                Container::new(
+                    Text::new("Online")
+                        .width(Length::Fill)
+                        .height(Length::Fill)
+                        .vertical_alignment(VerticalAlignment::Center))
+                    .style(ContainerStyle::Border)
+                    .width(Length::Fill)
+                    .padding(5)
+            )
+            .push(
                 Row::new()
                     .width(Length::Fill)
                     .spacing(8)
                     .align_items(Align::Center)
-                    .push(Text::new("Online")
-                        .width(Length::Fill)
-                        .vertical_alignment(VerticalAlignment::Center)
+                    .push(
+                        Button::new(&mut self.refresh_button,
+                                    Text::new("Refresh")
+                                        .width(Length::Fill)
+                                        .horizontal_alignment(HorizontalAlignment::Center))
+                            .on_press(Message::IncrementPressed)
+                            .width(Length::Fill)
                     )
-                    .push(Button::new(&mut self.refresh_button, Text::new("Refresh"))
-                        .on_press(Message::IncrementPressed)
+                    .push(
+                        Button::new(&mut self.shutdown_button,
+                                    Text::new("Power Off")
+                                        .width(Length::Fill)
+                                        .horizontal_alignment(HorizontalAlignment::Center))
+                            .on_press(Message::IncrementPressed)
+                            .width(Length::Fill)
                     )
-                    .push(Button::new(&mut self.shutdown_button, Text::new("Power Off"))
-                        .on_press(Message::IncrementPressed)
-                    )
-                    .push(Button::new(&mut self.settings_button, Text::new("Settings"))
-                        .on_press(Message::IncrementPressed)
+                    .push(
+                        Button::new(&mut self.settings_button,
+                                    Text::new("Settings")
+                                        .width(Length::Fill)
+                                        .horizontal_alignment(HorizontalAlignment::Center))
+                            .on_press(Message::IncrementPressed)
+                            .width(Length::Fill)
                     )
             )
             .push(
-                Row::new()
-                    .spacing(8)
-                    .push(
-                        Column::new()
-                            .spacing(8)
-                            .width(Length::Fill)
-                            .push(Button::new(&mut self.start_client_button,
-                                              Text::new("Start Client")
-                                                  .width(Length::Fill)
-                                                  .horizontal_alignment(HorizontalAlignment::Center))
-                                    .width(Length::Fill)
-                                    .on_press(Message::DecrementPressed)
-                            )
-                            .push(
-                                Container::new(Space::new(Length::Fill, Length::Fill))
-                                    .width(Length::Fill)
-                                    .height(Length::Fill)
-                                    .style(ContainerStyle::Colored(Color::from_rgb8(0, 230, 0)))
-                            )
-
-                    )
-                    .push(
-                        Column::new()
-                            .spacing(8)
-                            .width(Length::Fill)
-                            .push(Button::new(&mut self.start_server_button,
-                                              Text::new("Start Server")
-                                                  .width(Length::Fill)
-                                                  .horizontal_alignment(HorizontalAlignment::Center))
-                                .width(Length::Fill)
-                                .on_press(Message::DecrementPressed)
-                            )
-                            .push(Container::new(
-                               // Scrollable::new(&mut self.server_output)
-                               //     .push(Text::new("Test").size(50))
-                               //     .push(Text::new("Iced supports scrollable content. Try it out! Find the button further below."))
-                               //     .push(Text::new("Tip: You can use the scrollbar to scroll down faster!").size(16))
-                               //     .push(Column::new().height(Length::Units(4096)))
-                               //     .push(Text::new("You are halfway there!").width(Length::Fill).size(30).horizontal_alignment(HorizontalAlignment::Center))
-                               //     .push(Column::new().height(Length::Units(4096)))
-                               //     .push(Text::new("You made it!").width(Length::Fill).size(50).horizontal_alignment(HorizontalAlignment::Center))
-                               self.lines
-                                   .iter()
-                                   .map(|l|Text::new(l))
-                                   .fold(Scrollable::new(&mut self.server_output), |a, t|a.push(t))
-                            ).style(ContainerStyle::Border).padding(8))
-                    )
+                Container::new(
+                    Space::new(Length::Fill, Length::Fill))
+                    .style(ContainerStyle::Border)
+                    .width(Length::Fill)
+                    .height(Length::Fill)
             )
             .into()
-        //Column::new()
-        //    .padding(20)
-        //    .align_items(Align::Center)
-        //    .push(
-        //        Button::new(&mut self.increment_button, Text::new("Increment"))
-        //            .on_press(Message::IncrementPressed),
-        //    )
-        //    .push(Text::new(self.value.to_string()).size(50))
-        //    .push(
-        //        Button::new(&mut self.decrement_button, Text::new("Decrement"))
-        //            .on_press(Message::DecrementPressed),
-        //    )
-        //    .into()
     }
 }
