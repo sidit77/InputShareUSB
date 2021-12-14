0d9b761259a8e607560b9c80647bc2ee6776885f
sidit77 <sidit77@gmail.com>
2021-12-14 14:53:07 +0100

    signal handler

--- inputshare-server/src/main.rs
+++ inputshare-server/src/main.rs
@@ -4,6 +4,7 @@ use std::time::Duration;
 use mio::{Events, Interest, Poll, Token};
 use anyhow::Result;
 use mio::net::UdpSocket;
+use mio_signals::{Signal, Signals, SignalSet};
 use mio_timerfd::{ClockId, TimerFd};
 
 fn main() -> Result<()>{
@@ -17,15 +18,18 @@ fn main() -> Result<()>{
     let mut socket = UdpSocket::bind(SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), 12345))?;
     println!("running on {}", socket.local_addr()?);
 
+    let mut signals = Signals::new(SignalSet::all())?;
+
     let mut timer = TimerFd::new(ClockId::Monotonic)?;
     timer.set_timeout(&Duration::from_secs(1))?;
 
     const SERVER: Token = Token(0);
     const HEARTBEAT: Token = Token(1);
+    const SIGNAL: Token = Token(2);
     poll.registry().register(&mut socket, SERVER, Interest::READABLE)?;
     poll.registry().register(&mut timer, HEARTBEAT, Interest::READABLE)?;
-
-    loop {
+    poll.registry().register(&mut signals, SIGNAL, Interest::READABLE)?;
+    'outer: loop {
         poll.poll(&mut events, None)?;
         for event in events.iter() {
             match event.token() {
@@ -42,27 +46,23 @@ fn main() -> Result<()>{
                 HEARTBEAT => {
                     println!("Tick!");
                     timer.set_timeout(&Duration::from_millis(1000))?;
+                },
+                SIGNAL => loop {
+                    match signals.receive()? {
+                        Some(Signal::Interrupt) => break 'outer,
+                        Some(Signal::Quit) => break 'outer,
+                        Some(Signal::Terminate) => break 'outer,
+                        Some(_) => continue,
+                        None => break
+                    }
                 }
                 _ => {}
             }
         }
     }
 
-    /*
-    let mut socket = UdpSocket::bind("0.0.0.0:12345")?;
+    println!("Shutting down");
 
-    println!("running on {}", socket.local_addr()?);
+    Ok(())
 
-    let mut buffer = [0u8; 1500];
-    loop {
-        loop {
-            match socket.recv_from(&mut buffer) {
-                Ok((size, src)) => println!("Got {:?} from {}", &buffer[..size], src),
-                Err(e) if e.kind() == ErrorKind::WouldBlock => break,
-                Err(e) => Err(e)?
-            }
-        }
-    }
-
-     */
 }
