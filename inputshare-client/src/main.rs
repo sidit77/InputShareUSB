0c62c356b1f3150aa673f394205952f219cdd287
sidit77 <sidit77@gmail.com>
2021-12-15 20:35:48 +0100

    moved yawi to a new location

--- inputshare-client/src/main.rs
+++ inputshare-client/src/main.rs
@@ -1,6 +1,3 @@
-mod error;
-mod enums;
-
 use std::{mem, ptr};
 use std::convert::TryInto;
 use std::ffi::OsStr;
@@ -20,8 +17,6 @@ use winapi::um::processthreadsapi::GetCurrentThreadId;
 use winapi::um::winbase::{INFINITE, WAIT_OBJECT_0, WAIT_FAILED};
 use winapi::um::winuser::{CallNextHookEx, CreateWindowExW, CS_HREDRAW, CS_OWNDC, CS_VREDRAW, DefWindowProcW, DispatchMessageW, HWND_MESSAGE, KBDLLHOOKSTRUCT, LLKHF_EXTENDED, MapVirtualKeyW, MAPVK_VK_TO_VSC_EX, MSG, MsgWaitForMultipleObjects, PeekMessageW, PM_REMOVE, PostMessageW, PostThreadMessageW, QS_ALLINPUT, RegisterClassW, SetWindowsHookExW, TranslateMessage, UnhookWindowsHookEx, VK_NUMLOCK, VK_PAUSE, VK_SCROLL, VK_SNAPSHOT, WH_KEYBOARD_LL, WM_KEYDOWN, WM_KEYUP, WM_QUIT, WM_SYSKEYDOWN, WM_SYSKEYUP, WM_USER, WNDCLASSW};
 use winsock2_extensions::{NetworkEvents, WinSockExt};
-use crate::enums::{InputEvent, KeyState, VirtualKey, WindowsScanCode};
-
 
 fn main() -> Result<()>{
     {
@@ -148,69 +143,5 @@ fn create_window(name: &str) -> HWND {
             null_mut());
 
         handle
-    }
-}
-
-fn check<T>(ptr: *mut T) -> error::Result<*mut T>{
-    if ptr.is_null() {
-        Err(error::Error::last())
-    } else {
-        Ok(ptr)
-    }
-}
-
-const IGNORE: usize = 0x1234567;
-
-static mut HOOK: Option<HHOOK> = None;
-
-unsafe extern "system" fn low_level_keyboard_proc(code: raw::c_int, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
-    if code >= 0 {
-        let key_struct = *(lparam as *const KBDLLHOOKSTRUCT);
-
-        if key_struct.dwExtraInfo != IGNORE {
-
-            let event = match parse_virtual_key(&key_struct) {
-                Some(key) => match parse_key_state(wparam) {
-                    Some(state) => Some(InputEvent::KeyboardKeyEvent(key, parse_scancode(&key_struct), state)),
-                    None => {println!("Unknown event: {}", wparam); None}
-                }
-                None => {println!("Unknown key: {}", key_struct.vkCode); None}
-            };
-
-            if let Some(event) = event {
-                println!("{:?}", event);
-                PostMessageW(ptr::null_mut(), WM_USER, 0, 0);
-            }
-
-        }
-
-    }
-    CallNextHookEx(HOOK.unwrap(), code, wparam, lparam)
-}
-
-fn parse_scancode(key_struct: &KBDLLHOOKSTRUCT) -> WindowsScanCode {
-    let mut scancode = key_struct.scanCode as WindowsScanCode;
-    let vk = key_struct.vkCode as i32;
-    if scancode == 0x0 || vk == VK_SNAPSHOT || vk == VK_SCROLL || vk == VK_PAUSE || vk == VK_NUMLOCK {
-        scancode = unsafe {MapVirtualKeyW(key_struct.vkCode, MAPVK_VK_TO_VSC_EX)} as WindowsScanCode;
-    } else {
-        if key_struct.flags & LLKHF_EXTENDED == LLKHF_EXTENDED {
-            scancode |= 0xe000;
-        }
-    }
-    scancode
-}
-
-fn parse_virtual_key(key_struct: &KBDLLHOOKSTRUCT) -> Option<VirtualKey> {
-    key_struct.vkCode.try_into().ok()
-}
-
-fn parse_key_state(wparam: WPARAM) -> Option<KeyState> {
-    match wparam as UINT {
-        WM_KEYDOWN    => Some(KeyState::Pressed),
-        WM_SYSKEYDOWN => Some(KeyState::Pressed),
-        WM_KEYUP      => Some(KeyState::Released),
-        WM_SYSKEYUP   => Some(KeyState::Released),
-        _ => None
     }
 }
