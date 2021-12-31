d518530f7faf9f5b589dfa611177ee81a6ffc549
sidit77 <sidit77@gmail.com>
2021-12-31 10:01:48 +0100

    removed dead code

--- lib/inputshare-common/src/lib.rs
+++ lib/inputshare-common/src/lib.rs
@@ -1,7 +1,3 @@
-use std::net::TcpStream;
-use std::io::{Read, Write, ErrorKind, Error};
-use std::borrow::Cow;
-use std::convert::TryInto;
 use std::fmt::Debug;
 use num_enum::{FromPrimitive, IntoPrimitive, TryFromPrimitive};
 
@@ -259,46 +255,3 @@ pub enum HidKeyCode {
     MediaRefresh = 0xfa,
     MediaCalc = 0xfb,
 }
-
-
-pub struct PackageIds {}
-impl PackageIds {
-    pub const KEYBOARD: u8 = 0x1;
-    pub const MOUSE: u8 = 0x2;
-    pub const SWITCH: u8 = 0x3;
-}
-
-
-pub trait ReadExt: Read {
-
-    fn read_u16(&mut self) -> std::io::Result<u16> {
-        let mut buf = [0; 2];
-        self.read_exact(&mut buf)?;
-        Ok(u16::from_be_bytes(buf))
-    }
-
-    fn read_string<'a>(&mut self, buf: &'a mut [u8]) -> std::io::Result<Cow<'a, str>> {
-        let size = self.read_u16()? as usize;
-        self.read_exact(&mut buf[0..size])?;
-        Ok(String::from_utf8_lossy(&buf[0..size]))
-    }
-
-}
-
-pub trait WriteExt: Write {
-    fn write_u16(&mut self, value: u16) -> std::io::Result<()> {
-        let b = value.to_be_bytes();
-        self.write_all(&b[..])
-    }
-    fn write_string(&mut self, value: &str) -> std::io::Result<()>{
-        let b = value.as_bytes();
-        match TryInto::<u16>::try_into(b.len()){
-            Ok(l) => self.write_u16(l),
-            Err(err) => Err(Error::new(ErrorKind::InvalidData, err))
-        }?;
-        self.write_all(b)
-    }
-}
-
-impl ReadExt for TcpStream {}
-impl WriteExt for TcpStream {}
