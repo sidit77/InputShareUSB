8fcc975bd49e7ec83562b12d6eab12a3df6ec75c
sidit77 <sidit77@gmail.com>
2023-03-10 23:12:13 +0100

    basic egui application

--- inputshare-client/src/main.rs
+++ inputshare-client/src/main.rs
@@ -1,7 +1,20 @@
 #![windows_subsystem = "windows"]
 
+mod windows;
+
+use std::time::Instant;
 use anyhow::Result;
+use egui::Visuals;
+use error_tools::log::LogResultExt;
 use log::LevelFilter;
+use winit::dpi::PhysicalSize;
+use winit::event::{Event, StartCause, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::platform::run_return::EventLoopExtRunReturn;
+use winit::platform::windows::{IconExtWindows, WindowBuilderExtWindows};
+use winit::window::{Icon, WindowBuilder};
+use egui_d3d11::EguiD3D11;
+use crate::windows::{com_initialized, Direct3D};
 
 fn main() -> Result<()>{
     error_tools::gui::set_gui_panic_hook();
@@ -13,6 +26,78 @@ fn main() -> Result<()>{
         .parse_default_env()
         .init();
 
-    log::info!("hello world");
+    com_initialized();
+
+    let mut event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("InputShare Client")
+        .with_drag_and_drop(false)
+        .with_window_icon(Some(Icon::from_resource(32512, None)?))
+        .with_inner_size(PhysicalSize::new(400, 300))
+        .build(&event_loop)?;
+
+    let mut d3d = Direct3D::new(&window)?;
+    let mut egui_d3d = EguiD3D11::new(&event_loop, d3d.device.clone(), d3d.context.clone());
+    egui_d3d.egui_ctx.set_visuals(Visuals::light());
+
+    event_loop.run_return(move |event, _, control_flow| {
+        let mut redraw = || {
+
+            let repaint_after = egui_d3d.run(&window, |egui_ctx| {
+                egui::CentralPanel::default().show(egui_ctx, |ui| {
+                    ui.centered_and_justified(|ui| {
+                        ui.heading("Hello world");
+                    })
+                });
+            });
+
+            *control_flow = if repaint_after.is_zero() {
+                window.request_redraw();
+                ControlFlow::Poll
+            } else if let Some(repaint_after_instant) = Instant::now().checked_add(repaint_after) {
+                ControlFlow::WaitUntil(repaint_after_instant)
+            } else {
+                ControlFlow::Wait
+            };
+
+            {
+                unsafe {
+                    d3d.context.OMSetRenderTargets(Some(&[d3d.render_target().clone()]), None);
+                }
+
+                egui_d3d.paint(&window);
+
+                unsafe {
+                    d3d.swap_chain.Present(1, 0)
+                        .ok().unwrap();
+                }
+            }
+        };
+
+        match event {
+            Event::RedrawEventsCleared if cfg!(windows) => redraw(),
+            Event::RedrawRequested(_) if !cfg!(windows) => redraw(),
+
+            Event::WindowEvent { event, .. } => {
+                if matches!(event, WindowEvent::CloseRequested | WindowEvent::Destroyed) {
+                    *control_flow = ControlFlow::Exit;
+                }
+                if let WindowEvent::Resized(size) = &event {
+                    d3d.resize(size.width, size.height)
+                        .log_ok("Can not resize resources");
+                    //log::trace!("Resized dx resources to {}/{}", size.width, size.height);
+                } else if let WindowEvent::ScaleFactorChanged { .. } = &event {
+                    log::error!("Need to resize");
+                }
+
+                let event_response = egui_d3d.on_event(&event);
+                if event_response.repaint {
+                    window.request_redraw();
+                }
+            }
+            Event::NewEvents(StartCause::ResumeTimeReached { .. }) => window.request_redraw(),
+            _ => (),
+        }
+    });
     Ok(())
 }
--- inputshare-client/src/windows.rs
+++ inputshare-client/src/windows.rs
@@ -1,3 +1,135 @@
+use std::marker::PhantomData;
+use anyhow::Result;
+use error_tools::SomeOptionExt;
+use windows::core::Interface;
+use windows::Win32::Foundation::HWND;
+use windows::Win32::Graphics::Direct3D11::*;
+use windows::Win32::Graphics::Direct3D::*;
+use windows::Win32::Graphics::Dxgi::*;
+use windows::Win32::Graphics::Dxgi::Common::*;
+use windows::Win32::System::Com::{CoUninitialize, CoInitializeEx, COINIT_MULTITHREADED};
+use windows::Win32::UI::HiDpi::{SetProcessDpiAwarenessContext, DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2};
+use winit::platform::windows::WindowExtWindows;
+use winit::window::Window;
+
+pub struct Direct3D {
+    pub device: ID3D11Device,
+    pub context: ID3D11DeviceContext4,
+    pub swap_chain: IDXGISwapChain1,
+    render_target: Option<ID3D11RenderTargetView>
+}
+
+impl Direct3D {
+
+    pub fn new(window: &Window) -> Result<Self> {
+        let mut d3d_device = None;
+        let mut d3d_ctx = None;
+        unsafe {
+            D3D11CreateDevice(
+                None,
+                D3D_DRIVER_TYPE_HARDWARE,
+                None,
+                D3D11_CREATE_DEVICE_FLAG(0),
+                Some(&[D3D_FEATURE_LEVEL_11_1]),
+                D3D11_SDK_VERSION,
+                Some(&mut d3d_device),
+                None,
+                Some(&mut d3d_ctx),
+            )?;
+        }
+        let d3d_device = d3d_device.some()?;
+        let d3d_ctx = d3d_ctx.some()?.cast::<ID3D11DeviceContext4>()?;
+
+        let dxgi_factory = unsafe { CreateDXGIFactory1::<IDXGIFactory2>()? };
+        let window_size = window.inner_size();
+        let swap_chain = unsafe {
+            dxgi_factory.CreateSwapChainForHwnd(
+                &d3d_device,
+                HWND(window.hwnd() as _),
+                &DXGI_SWAP_CHAIN_DESC1 {
+                    Width: window_size.width,
+                    Height: window_size.height,
+                    Format: DXGI_FORMAT_R8G8B8A8_UNORM,
+                    BufferCount: 2,
+                    BufferUsage: DXGI_USAGE_RENDER_TARGET_OUTPUT,
+                    SwapEffect: DXGI_SWAP_EFFECT_FLIP_DISCARD,
+                    Scaling: DXGI_SCALING_NONE,
+                    SampleDesc: DXGI_SAMPLE_DESC {
+                        Count: 1,
+                        Quality: 0,
+                    },
+                    ..Default::default()
+                },
+                None,
+                None,
+            )?
+        };
+
+        let rtv = unsafe {
+            let buffer = swap_chain.GetBuffer::<ID3D11Texture2D>(0)?;
+            let mut target = std::mem::zeroed();
+            d3d_device.CreateRenderTargetView(&buffer, None, Some(&mut target))?;
+            target.some()?
+        };
+
+        Ok(Self {
+            device: d3d_device,
+            context: d3d_ctx,
+            swap_chain,
+            render_target: Some(rtv),
+        })
+    }
+
+    pub fn render_target(&self) -> &ID3D11RenderTargetView {
+        self.render_target.as_ref()
+            .expect("The rendertarget should never not be initialized")
+    }
+
+    pub fn resize(&mut self, width: u32, height: u32) -> Result<()> {
+        unsafe {
+            self.context.OMSetRenderTargets(None, None);
+            self.render_target = None;
+            self.swap_chain.ResizeBuffers(0, width, height, DXGI_FORMAT_UNKNOWN, 0)?;
+            let buffer = self.swap_chain.GetBuffer::<ID3D11Texture2D>(0)?;
+            self.device.CreateRenderTargetView(&buffer, None, Some(&mut self.render_target))?;
+            Ok(())
+        }
+    }
+
+}
+
+#[derive(Default)]
+struct ComWrapper {
+    _ptr: PhantomData<*mut ()>,
+}
+
+thread_local!(static COM_INITIALIZED: ComWrapper = {
+    unsafe {
+        SetProcessDpiAwarenessContext(Some(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2));
+        CoInitializeEx(None, COINIT_MULTITHREADED)
+            .expect("Could not initialize COM");
+        let thread = std::thread::current();
+        log::trace!("Initialized COM on thread \"{}\"", thread.name().unwrap_or(""));
+        ComWrapper::default()
+    }
+});
+
+impl Drop for ComWrapper {
+    fn drop(&mut self) {
+        unsafe {
+            CoUninitialize();
+            let thread = std::thread::current();
+            log::trace!("Uninitialized COM on thread \"{}\"", thread.name().unwrap_or(""));
+        }
+    }
+}
+
+#[inline]
+pub fn com_initialized() {
+    COM_INITIALIZED.with(|_| {});
+}
+
+/*
 use std::convert::TryInto;
 use std::mem;
 use std::ptr::{null, null_mut};
@@ -38,3 +170,4 @@ pub fn get_message() -> Option<MSG> {
         }
     }
 }
+*/
