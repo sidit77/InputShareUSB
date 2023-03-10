197103b57e5dbc3ce1a788e08ee8fb8fcab07d43
sidit77 <sidit77@gmail.com>
2023-03-10 22:47:43 +0100

    some cleanup to start with a fresh ui based on egui

--- inputshare-client/build.rs
+++ inputshare-client/build.rs
@@ -1,14 +1,5 @@
-#[cfg(windows)]
-extern crate winres;
-
-#[cfg(windows)]
 fn main() {
-    let mut res = winres::WindowsResource::new();
+    let mut res = tauri_winres::WindowsResource::new();
     res.set_icon("icon.ico");
     res.compile().unwrap();
-}
-
-#[cfg(not(windows))]
-fn main() {
-
 }
