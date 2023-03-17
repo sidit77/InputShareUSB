dc5691a8412a742608eac0f09f350442bf814cdf
sidit77 <sidit77@gmail.com>
2023-03-17 10:32:34 +0100

    experimental switch to druid because winit doesnt play nice keyboard hooks

--- inputshare-client/build.rs
+++ inputshare-client/build.rs
@@ -1,5 +1,5 @@
 fn main() {
     let mut res = tauri_winres::WindowsResource::new();
-    res.set_icon("icon.ico");
+    res.set_icon_with_id("icon.ico", "1");
     res.compile().unwrap();
 }
--- inputshare-client/src/main.rs
+++ inputshare-client/src/main.rs
@@ -1,66 +1,34 @@
 #![windows_subsystem = "windows"]
 
-mod windows;
-mod ui;
+use druid::widget::{Label};
+use druid::{AppLauncher, Widget, WidgetExt, WindowDesc};
+use yawi::InputHook;
 
-use std::time::Instant;
-use anyhow::Result;
-use egui::{Visuals};
-use log::LevelFilter;
-use winit::dpi::PhysicalSize;
-use winit::event_loop::{ControlFlow, EventLoop};
-use winit::platform::run_return::EventLoopExtRunReturn;
-use winit::platform::windows::{IconExtWindows, WindowBuilderExtWindows};
-use winit::window::{Icon, WindowBuilder};
-use crate::ui::EguiWindow;
-use crate::windows::{com_initialized};
-
-fn main() -> Result<()>{
-    env_logger::builder()
-        .filter_level(LevelFilter::Trace)
-        .format_timestamp(None)
-        //.format_target(false)
-        .parse_default_env()
-        .init();
-
+pub fn main() {
     #[cfg(not(debug_assertions))]
     error_tools::gui::set_gui_panic_hook();
-    com_initialized();
 
-    let mut event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_title("InputShare Client")
-        .with_drag_and_drop(false)
-        .with_window_icon(Some(Icon::from_resource(32512, None)?))
-        .with_inner_size(PhysicalSize::new(400, 300))
-        .build(&event_loop)?;
+    let window = WindowDesc::new(make_ui())
+        .window_size((400.0, 300.0))
+        .title("InputShare Client");
 
-    let mut window = EguiWindow::new(window, &event_loop);
-    window.ctx().set_visuals(Visuals::light());
+    let launcher = AppLauncher::with_window(window);
 
-    event_loop.run_return(move |event, _, control_flow| {
-        let close_requested = window.handle_events(&event, |ctx| {
-            egui::CentralPanel::default().show(ctx, |ui| {
-                ui.centered_and_justified(|ui| {
-                    ui.horizontal(|ui| {
-                        ui.heading("Hello world");
-                        ui.spinner();
-                    });
-                });
-            });
+    let event_sink = launcher.get_external_handle();
+    let _hook = InputHook::register(move |event| {
+        event_sink.add_idle_callback(move |data: &mut String| {
+            *data = format!("{:?}", event);
         });
-        if close_requested {
-            *control_flow = ControlFlow::Exit;
-        }
-        if !matches!(*control_flow, ControlFlow::ExitWithCode(_)) {
-            let next_update = window.next_repaint();
-            *control_flow = match next_update {
-                None => ControlFlow::Wait,
-                Some(deadline) if deadline <= Instant::now() => ControlFlow::Poll,
-                Some(deadline) => ControlFlow::WaitUntil(deadline)
-            }
-        }
-    });
-    Ok(())
+        true
+    }).unwrap();
+
+    launcher
+        .log_to_console()
+        .launch(String::from("None"))
+        .expect("launch failed");
 }
 
+fn make_ui() -> impl Widget<String> {
+    Label::dynamic(|data: &String, _| data.clone())
+        .center()
+}
